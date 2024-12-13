2b2709842467590967acc601b3cef0e6c958de88
Joseph <Josph@com.pl>
2024-12-13 13:39:09 +0100

    stworzenie interface i stworzenie orzel i resztka

---
+++ app/src/main/java/com/example/a7trudnychaplikacji/CoinOrDiceRollSimulator.kt
@@ -0,0 +1,35 @@
+package com.example.a7trudnychaplikacji
+import kotlin.random.Random
+
+interface RollingTool{
+    fun roll()
+}
+
+class Coin: RollingTool{
+    override fun roll(){
+        val random= Random.nextInt(2)
+        val wyborKomputera=when(random){
+            0->"Heads"
+            1->"Tails"
+            else->"Bład"
+        }
+        println("Dokonaj wyboru(Heads or Tails):")
+        var wybor= readLine()?.trim()
+
+        println("Komputer wybral: ${wyborKomputera}")
+        if (wybor != "Heads" && wybor != "Tails") {
+            println("Błędny wybór. Proszę wybrać Heads lub Tails.")
+            return
+        }
+        val wynik= when{
+            wybor=="Heads" && wyborKomputera=="Tails"->"Wygrales"
+            wybor=="Tails" && wyborKomputera=="Heads"->"przegrales"
+            else->"Zostala wybrana ta sama wartosc"
+        }
+        println(wynik)
+    }
+}
+fun main(){
+    val Coin=Coin()
+    Coin.roll()
+}
