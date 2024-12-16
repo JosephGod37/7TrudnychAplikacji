0e86e6c9a8e8576b2ad9584e6e72d0d2ec57eda6
Joseph <Joseph@gmail.com>
2024-12-16 19:23:47 +0100

    KnowledgeQuiz stworzenie data class i stworzenie listy dla wielokrotnego wyboru

--- app/src/main/java/com/example/a7trudnychaplikacji/KnowledgeQuiz.kt
+++ app/src/main/java/com/example/a7trudnychaplikacji/KnowledgeQuiz.kt
@@ -5,3 +5,54 @@ interface pytanie{
     fun sprawdzanieOdp()
 }
 
+data class PytaniaDoWielokrotnegoWyboru(
+    val pytanie: String,
+    val a: String,
+    val b:String,
+    val c:String,
+    val d:String,
+    val poprawnaOdpowiedz:String
+)
+
+val pytaniaWkw= listOf(
+    PytaniaDoWielokrotnegoWyboru(
+        "Co jest największą planetą w Układzie Słonecznym?",
+        "Mars",
+        "Ziemia",
+        "Jowisz",
+        "Wenus",
+         "c"
+    ),
+    PytaniaDoWielokrotnegoWyboru(
+         "Który język programowania jest oficjalny dla Androida?",
+         "Java",
+         "Kotlin",
+         "Python",
+        "C++",
+         "b"
+    ),
+    PytaniaDoWielokrotnegoWyboru(
+         "Ile wynosi pierwiastek kwadratowy z 16?",
+        "2",
+         "4",
+         "8",
+         "16",
+         "b"
+    ),
+    PytaniaDoWielokrotnegoWyboru(
+         "Który kraj ma najwięcej ludności na świecie?",
+         "Indie",
+         "Chiny",
+         "USA",
+         "Indonezja",
+         "b"
+    ),
+    PytaniaDoWielokrotnegoWyboru(
+         "Który pierwiastek chemiczny ma symbol 'O'?",
+         "Tlen",
+         "Wodór",
+         "Azot",
+         "Hel",
+         "a"
+    )
+)
